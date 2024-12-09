2562f3239fa59af7038fd4a2e1414c3d98be541a
sv742744 <shashi09op@gmail.com>
2024-12-09 15:19:09 +0530

    Add files via upload

---
+++ minvalueinmatrix.java
@@ -0,0 +1,35 @@
+/*
+3
+3
+5 6 7 
+3 4 5 
+2 6 7 
+2
+*/
+import java.util.*;
+public class minvalueinmatrix {
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+    int r=sc.nextInt();
+    int c=sc.nextInt();
+    int arr[][]=new int[r][c];
+    for(int i=0;i<r;i++)
+    {
+      for(int j=0;j<c;j++)
+      {
+        arr[i][j]=sc.nextInt();
+      }
+    }
+    int min=arr[0][0];
+    for(int i=0;i<r;i++)
+    {
+      for(int j=0;j<c;j++)
+      {
+       if(arr[i][j]<min)
+            min=arr[i][j];
+      }
+    }
+     System.out.println(min);
+  }
+      
+} 
